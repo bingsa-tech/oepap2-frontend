711e917d15a65385a143debe175533fc451b5ced
bingsa-tech <biljobs@outlook.com>
2025-12-13 15:16:48 -0500

    Add About route to the router configuration
    Add About page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+import About from '../views/About.vue';
 import Login from '../views/auth/Login.vue';
 import Register from '../views/auth/Register.vue';
 import Profile from '../views/Profile.vue';
@@ -13,6 +14,7 @@ import News from '../views/News.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
+  { path: '/about', name: 'About', component: About },
   { path: '/login', name: 'Login', component: Login, meta: { guest: true } },
   { path: '/register', name: 'Register', component: Register, meta: { guest: true } },
   
